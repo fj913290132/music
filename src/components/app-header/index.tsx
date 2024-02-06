c97eac278c627543ac8eecfe804354dedd1c2a40
fengjie <fengjie@dip-ai.onaliyun.com>
2024-02-06 18:38:55 +0800

    header

--- src/components/app-header/index.tsx
+++ src/components/app-header/index.tsx
@@ -1,13 +1,64 @@
 import React, { memo } from 'react'
-import { AppHeaderWrapper } from './style'
+import { AppHeaderWrapper, HeaderLeft, HeaderRight } from './style'
+import { NavLink } from 'react-router-dom'
+import headerTitles from '@/assets/data/header_titles.json'
+
+import { Input } from 'antd'
+import { SearchOutlined } from '@ant-design/icons'
 interface IProps {
   children?: React.ReactNode
 }
 
 const appHeader: React.FC<IProps> = () => {
+  function showItem(item: any) {
+    if (item.type === 'path') {
+      return (
+        <NavLink
+          to={item.link}
+          className={({ isActive }) => {
+            return isActive ? 'active' : undefined
+          }}
+        >
+          {item.title}
+          <i className="icon sprite_01"></i>
+        </NavLink>
+      )
+    } else {
+      return (
+        <a href={item.link} rel="noreferrer" target="_blank">
+          {item.title}
+        </a>
+      )
+    }
+  }
   return (
     <AppHeaderWrapper>
-      <div>appHeader</div>
+      <div className="content wrap-v1">
+        <HeaderLeft>
+          <a className="logo sprite_01" href="/">
+            网易云音乐
+          </a>
+          <div className="title-list">
+            {headerTitles.map((item) => {
+              return (
+                <div className="item" key={item.title}>
+                  {showItem(item)}
+                </div>
+              )
+            })}
+          </div>
+        </HeaderLeft>
+        <HeaderRight>
+          <Input
+            className="search"
+            placeholder="音乐/视频/电台/用户"
+            prefix={<SearchOutlined />}
+          />
+          <span className="center">创作者中心</span>
+          <span className="login">登录</span>
+        </HeaderRight>
+      </div>
+      <div className="divider"></div>
     </AppHeaderWrapper>
   )
 }

05d4046aa9f0798f9417299f050f2564ec4a1b77
fengjie <fengjie@dip-ai.onaliyun.com>
2024-02-07 18:32:52 +0800

    diptach两种方式

--- src/views/discover/children-views/recommend/store/index.ts
+++ src/views/discover/children-views/recommend/store/index.ts
@@ -1,13 +1,21 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import type { BannerRoot, initialStateType } from '../type'
 import { getBanners } from '../service'
+//第一种方法
+// export const fetchBannersDataAction = createAsyncThunk('banners', async () => {
+//   const res: BannerRoot = await getBanners()
+//   console.log('fetchBannersDataAction', res)
+//   return res
+// })
 
-export const fetchBannersDataAction = createAsyncThunk('banners', async () => {
-  const res: BannerRoot = await getBanners()
-  console.log('fetchBannersDataAction', res)
-  return res
-})
-
+export const fetchBannersDataAction = createAsyncThunk(
+  'banners',
+  async (payload, { dispatch }) => {
+    const res: BannerRoot = await getBanners()
+    dispatch(setBanners(res.banners))
+    console.log('fetchBannersDataAction', payload)
+  }
+)
 const initialState: initialStateType = {
   banners: []
 }
@@ -15,11 +23,17 @@ const initialState: initialStateType = {
 const recommendSlice = createSlice({
   name: 'recommend',
   initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder.addCase(fetchBannersDataAction.fulfilled, (state, { payload }) => {
-      state.banners = payload.banners
-    })
+  reducers: {
+    setBanners(state, { payload }) {
+      state.banners = payload
+    }
   }
+  //第一种方法
+  // extraReducers: (builder) => {
+  //   builder.addCase(fetchBannersDataAction.fulfilled, (state, { payload }) => {
+  //     state.banners = payload.banners
+  //   })
+  // }
 })
 export default recommendSlice.reducer
+export const { setBanners } = recommendSlice.actions
